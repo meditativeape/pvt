dd39fd4a559e1eb5570d65e96bad70ff0f3c803f
Beck Chen <csybeck@gmail.com>
2014-01-25 19:55:20 -0600

    Add process input in game server

--- server/game.server.js
+++ server/game.server.js
@@ -5,30 +5,62 @@ if( 'undefined' !== typeof global ){
     var helper = require("../shared/game.shared.helper.js");
 	var Point = helper.Point;
 	var CONSTANTS = helper.CONSTANTS;
-}
+};
 
 // Import UUID
 var UUID = require('node-uuid');
 
 var GameServer = function(){
-	pikachuPlayer = null;
-	trPlayer = null;
+	this.pikachuPlayer = null;
+	this.trPlayer = null;
+    
 	// Store game's uuid
 	this.id = UUID();
-}
+};
 
 GameServer.prototype.setPikachuPlayer = function(/*client*/ player){
-	pikachuPlayer = player;
-}
+	this.pikachuPlayer = player;
+};
 
 GameServer.prototype.setTRPlayer = function(/*client*/ player){
-	trPlayer = player;
-}
+	this.trPlayer = player;
+};
 
 GameServer.prototype.leaveGame = function(/*client*/ player){
 	//TODO
-}
+};
 
+GameServer.prototype.physicsUpdate = function(){
+    
+};
+
+GameServer.prototype.processInput = function(){
+    var pikachuInputsLength = this.pikachuPlayer.inputs.length;
+    if (pikachuInputsLength) {
+        var input;
+        for (input in this.pikachuPlayer.inputs) {
+            if (input.action === 'left') {
+                // do something
+            } else if (input.action === 'up') {
+                // do something
+            } else if (input.action === 'right') {
+                // do something
+            }
+        }
+        this.pikachuPlayer.lastInputTime = input.time;
+        this.pikachuPlayer.lastInputSeq = input.seq;
+    }
+    
+    var trInputsLength = this.trPlayer.inputs.length;
+    if (trInputsLength) {
+        var input;
+        for (input in this.trInputsLength.inputs) {
+            // do something
+        }
+        this.trPlayer.lastInputTime = input.time;
+        this.trPlayer.lastInputSeq = input.seq;
+    }
+};
 
 //Export GameServer
 module.exports = GameServer;
