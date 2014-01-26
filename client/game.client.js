f1392fab27d6fbdd1d504b8af6a1a0e53ff7732f
Beck Chen <csybeck@gmail.com>
2014-01-26 02:55:36 -0600

    Basic server and client functionality

--- client/game.client.js
+++ client/game.client.js
@@ -12,7 +12,7 @@ var GameClient = function(/*int*/type, /*socketIO*/ mainSocket) {
 	this.gameState = new GameState(this);
 
     this.gameState.platforms[0] = new Platform(new Point(CONSTANTS.pikachuStartX,CONSTANTS.pikachuStartY-30),new Point(-5,0),0);
-	 this.gameState.start();
+	this.gameState.start();
     
 	this.gameClientUI = new GameClientUI(this.gameState);
     this.gameClientUI.initialize();
@@ -36,6 +36,40 @@ GameClient.prototype.physicsUpdate = function(){
 };
 
 GameClient.prototype.handleMessage = function(/*string*/message){
+    var keywords = message.split(" ");
+    switch (keywords[1]) {
+    case "input":
+        if (this.type == 0) { // pikachu handles pokeball
+            var pokeballPos = new Point(parseInt(keywords[3]), parseInt(keywords[4]));
+            this.processTRInput(pokeballPos);
+        } else { // tr handles pikachu
+            this.processPikachuInput(keywords[2]);
+        }
+        break;
+    }
+};
+
+GameClient.prototype.processPikachuInput = function(/*String*/ action){
+    console.log(action);
+    switch(action) {
+    case "left":
+        this.gameState.pikachuBrake();
+        break;
+    case "right":
+        this.gameState.pikachuDash();
+        break;
+    case "up":
+        this.gameState.pikachuJump();
+        break;
+    case "stop":
+        this.gameState.pikachuNormal();
+        break;
+    default:
+    }
+};
+
+GameClient.prototype.processTRInput = function(/*Point*/ pokeballPos){
+    // TODO
 };
 
 GameClient.prototype.cleanUp = function(){
