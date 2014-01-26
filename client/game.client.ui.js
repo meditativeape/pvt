631121b0b4774a41088d1386f944faa05d7c51b2
Cindy Xu <cindy.yijia.xu@gmail.com>
2014-01-26 00:54:20 -0600

    add animation to the platform

--- client/game.client.ui.js
+++ client/game.client.ui.js
@@ -113,11 +113,13 @@ GameClientUI.prototype.draw = function(){
         stroke: 'black',
         strokeWidth: 4});
     this.platformLayer.add(rectPlat);
+    
     this.rectAnim = new Kinetic.Animation(function(frame){
     	var timeDiff = frame.timeDiff;
     	rectPlat.move({x: this.client.platforms[0].velocity.X* (timeDiff / 16), 
-                  y: 0});
-    }
+                  	   y: 0});
+	}, this.platformLayer);
+	this.rectAnim.start();
     
     
     // Create two Kinetic image objects for the scrolling floor
